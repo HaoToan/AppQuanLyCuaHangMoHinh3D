e02875a6c9f96c1455ab6268ce9ff18ca948371e
NgocPhu2001 <116866159+NgocPhu2001@users.noreply.github.com>
2022-12-04 16:25:57 +0700

    Update load image on item product
    Update load image on item product

--- QuanLySanPham.java
+++ QuanLySanPham.java
@@ -70,9 +70,27 @@ public class QuanLySanPham extends CapNhatSanPham {
         }
     }
 
-   
 
     private void addEvents() {
+        btnPL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                xulyPL();
+            }
+        });
+
+        btnThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                xulyThem();
+            }
+        });
+        btnAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                xulyALL();
+            }
+        });
         lvSP.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -338,6 +356,223 @@ public class QuanLySanPham extends CapNhatSanPham {
                 dialog.show();
             }
         });
+    }
+
+    private void xulyALL() {
+        String ma = "";
+        String ten = "";
+        String loai = "";
+        String tt = "";
+        int gia = 0;
+        if (Thongtin.swappage == true) {
+            if (Thongtin.function == true) {
+                if (Thongtin.module.equals("Them")) {
+                    ma = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getMasp().toString();
+                    ten = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getTensp().toString();
+                    loai = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getMaloaisp().toString();
+                    tt = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getThongtin().toString();
+                    gia = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getGia();
+                    Thongtin.arrayMoHinh.add(new SanPham(ma, ten, loai, tt, gia, ""));
+                    updateDatatoSever(url,ma,ten,loai,tt,String.valueOf(gia),"img");
+                    Thongtin.module = "";
+                } else if (Thongtin.module.equals("Sua")) {
+                    String madl = Thongtin.arrayLoaiSP.get(Thongtin.chon).getMasp().toString();
+                    ma = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getMasp().toString();
+                    ten = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getTensp().toString();
+                    loai = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getMaloaisp().toString();
+                    tt = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getThongtin().toString();
+                    gia = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getGia();
+                    for (int k = 0; k < Thongtin.arrayMoHinh.size(); k++) {
+                        if (Thongtin.arrayMoHinh.get(k).getMasp().equals(madl)) {
+                            Thongtin.arrayMoHinh.get(k).setMasp(ma);
+                            Thongtin.arrayMoHinh.get(k).setTensp(ten);
+                            Thongtin.arrayMoHinh.get(k).setMaloaisp(loai);
+                            Thongtin.arrayMoHinh.get(k).setThongtin(tt);
+                            Thongtin.arrayMoHinh.get(k).setGia(gia);
+                            if (Thongtin.checkdoianh == true) {
+                                Thongtin.arrayMoHinh.get(k).setAnh("");
+                            }
+                        }
+                    }
+                    Thongtin.module = "";
+                } else {
+                    lvSP.setAdapter(Thongtin.adapterSanPham);
+                }
+            } else {
+                if (Thongtin.module.equals("Them")) {
+                    ma = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getMasp().toString();
+                    ten = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getTensp().toString();
+                    loai = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getMaloaisp().toString();
+                    tt = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getThongtin().toString();
+                    gia = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getGia();
+                    Thongtin.arrayMoHinh.add(new SanPham(ma, ten, loai, tt, gia, ""));
+                    updateDatatoSever(url,ma,ten,loai,tt,String.valueOf(gia),"img");
+                    Thongtin.adapterSanPham.notifyDataSetChanged();
+                    Thongtin.module = "";
+                } else if (Thongtin.module.equals("Sua")) {
+                    ma = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getMasp().toString();
+                    ten = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getTensp().toString();
+                    loai = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getMaloaisp().toString();
+                    tt = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getThongtin().toString();
+                    gia = Thongtin.arrayUpdate.get(Thongtin.sp - 1).getGia();
+                    Thongtin.arrayMoHinh.get(Thongtin.chon).setMasp(ma);
+                    Thongtin.arrayMoHinh.get(Thongtin.chon).setTensp(ten);
+                    Thongtin.arrayMoHinh.get(Thongtin.chon).setMaloaisp(loai);
+                    Thongtin.arrayMoHinh.get(Thongtin.chon).setThongtin(tt);
+                    Thongtin.arrayMoHinh.get(Thongtin.chon).setGia(gia);
+                    Thongtin.adapterSanPham.notifyDataSetChanged();
+                    Thongtin.module = "";
+                } else {
+                    lvSP.setAdapter(Thongtin.adapterSanPham);
+                }
+            }
+            lvSP.setAdapter(Thongtin.adapterSanPham);
+        } else {
+            lvSP.setAdapter(Thongtin.adapterSanPham);
+        }
+
+        Thongtin.checkdoianh = false;
+        Thongtin.module = "";
+        Thongtin.function = false;
+        Toast.makeText(QuanLySanPham.this,"Tất cả SP",Toast.LENGTH_SHORT).show();
+    }
+
+    private void xulySua() {
+        Thongtin.module = "Sua";
+        Intent intent = new Intent(QuanLySanPham.this, CapNhatSanPham.class);
+        startActivity(intent);
+    }
+
+
+    private void xulyPL() {
+        Thongtin.function = true;
+        ListView lvLoaiSP;
+        Dialog dialog = new Dialog(QuanLySanPham.this);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.listviewloaisanpham);
+        lvLoaiSP = (ListView) dialog.findViewById(R.id.lvloai);
+        lvLoaiSP.setAdapter(Thongtin.adapterLoaiSP);
+        lvLoaiSP.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Dialog dialog1 = new Dialog(dialog.getContext());
+                dialog1.setCancelable(true);
+                dialog1.setContentView(R.layout.xulyloaisp_dialog);
+                ImageButton btnXem = (ImageButton) dialog1.findViewById(R.id.btnXem);
+                ImageButton btnXoa = (ImageButton) dialog1.findViewById(R.id.btnXoa);
+                btnXem.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Thongtin.arrayLoaiSP.clear();
+                        String ml = Thongtin.listLoai.get(i).toString();
+                        for (int a = 0; a < Thongtin.arrayMoHinh.size(); a++) {
+                            if (ml.equals(Thongtin.arrayMoHinh.get(a).getMaloaisp())) {
+                                String ma = Thongtin.arrayMoHinh.get(a).getMasp();
+                                String ten = Thongtin.arrayMoHinh.get(a).getTensp();
+                                String loai = Thongtin.arrayMoHinh.get(a).getMaloaisp();
+                                String tt = Thongtin.arrayMoHinh.get(a).getThongtin();
+                                int gia = Thongtin.arrayMoHinh.get(a).getGia();
+                                String anh = Thongtin.arrayMoHinh.get(a).getAnh();
+                                Thongtin.arrayLoaiSP.add(new SanPham(ma, ten, loai, tt, gia, anh));
+                            }
+                        }
+                        Thongtin.adapterSanPhamTheoloai.notifyDataSetChanged();
+                        lvSP.setAdapter(Thongtin.adapterSanPhamTheoloai);
+                        dialog1.cancel();
+                        dialog.cancel();
+                        Toast.makeText(QuanLySanPham.this,"Loại SP: "+ml,Toast.LENGTH_SHORT).show();
+                    }
+                });
+                btnXoa.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String ml=Thongtin.listLoai.get(i).getMaloaisp().toString();
+                        Dialog dialog2 = new Dialog(dialog1.getContext());
+                        dialog2.setCancelable(false);
+                        dialog2.setContentView(R.layout.yesno_dialog);
+                        Button btnyes = (Button) dialog2.findViewById(R.id.btnyes);
+                        Button btnno = (Button) dialog2.findViewById(R.id.btnno);
+                        btnyes.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Thongtin.listLoai.remove(i);
+                                for(int j=0;j<Thongtin.arrayMoHinh.size();j++){
+                                    if(Thongtin.arrayMoHinh.get(j).getMaloaisp().toString().equals(ml)){
+                                        Thongtin.arrayMoHinh.remove(j);
+                                        j--;
+                                    }
+                                }
+                                Thongtin.adapterLoaiSP.notifyDataSetChanged();
+                                Thongtin.adapterSanPham.notifyDataSetChanged();
+                                dialog2.cancel();
+                                dialog1.cancel();
+                                lvLoaiSP.setAdapter(Thongtin.adapterLoaiSP);
+                                Toast.makeText(QuanLySanPham.this,"Xóa thành công!",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        btnno.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                dialog2.cancel();
+                            }
+                        });
+                        dialog2.show();
+                    }
+                });
+                dialog1.show();
+            }
+        });
+        ImageButton btnnew = (ImageButton) dialog.findViewById(R.id.btnnew);
+        ImageButton btnback = (ImageButton) dialog.findViewById(R.id.btnback);
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
+        btnnew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog1= new Dialog(QuanLySanPham.this);
+                dialog1.setCancelable(false);
+                dialog1.setContentView(R.layout.newloai_dialog);
+                EditText etmaloai= (EditText) dialog1.findViewById(R.id.etMaloaisp);
+                ImageButton btnimgback=(ImageButton) dialog1.findViewById(R.id.btnimghuy);
+                ImageButton btndone=(ImageButton) dialog1.findViewById(R.id.btnimgthem);
+                btnimgback.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog1.cancel();
+                    }
+                });
+                btndone.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        boolean done=true;
+                        if(etmaloai.getText().length()==0){
+                            Toast.makeText(QuanLySanPham.this,"Vui lòng nhập thông tin!",Toast.LENGTH_SHORT).show();
+                        }else{
+                            for(int j=0;j<Thongtin.listLoai.size();j++){
+                                if(etmaloai.getText().toString().equals(Thongtin.listLoai.get(j).getMaloaisp().toString())){
+                                    done=false;
+                                }
+                            }
+                        }
+                        if(done==false){
+                            Toast.makeText(QuanLySanPham.this,"Loại SP đã có!",Toast.LENGTH_SHORT).show();
+                        }else{
+                            Thongtin.listLoai.add(new LoaiSanPham(etmaloai.getText().toString()));
+                            Thongtin.adapterLoaiSP.notifyDataSetChanged();
+                            Toast.makeText(QuanLySanPham.this,"Thêm thành công!",Toast.LENGTH_SHORT).show();
+                            dialog1.cancel();
+                            dialog.cancel();
+                        }
+                    }
+                });
+                dialog1.show();
+            }
+        });
+        dialog.show();
     }
 
 }
